af13a04f40ff1319f3fc52636f36413781f0675b
rk296425 <119296355+rk296425@users.noreply.github.com>
2023-02-03 11:37:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-01a4f99c4ac11b03c"
   instance_type = "t2.micro"
-  key_name = "rahamkeys"
+  key_name = "rkall"
   availabilty_zone = "us-east-1d"
   availability_zone = "us-east-1d"
   tags= {
